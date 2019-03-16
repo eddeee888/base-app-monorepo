ec0f53efd1158574763d91df61a0b0552130a8aa
Eddy Nguyen <eddeee.nguyen@gmail.com>
2019-03-16 14:24:10 +1100

    Use linkgen in AppRouter

--- client/src/AppRouter.tsx
+++ client/src/AppRouter.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import LayoutController from 'src/common/components/LayoutController';
+import { linkgen, Paths } from 'src/common/helpers/pathing';
 import Home from 'src/pages/Home';
 import Login from 'src/pages/Login';
 import Logout from 'src/pages/Logout';
 import Signup from 'src/pages/Signup';
-import LayoutController from './common/components/LayoutController';
 
 const AppRouter = () => (
   <BrowserRouter>
     <LayoutController>
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/login/" component={Login} />
-        <Route path="/signup/" component={Signup} />
-        <Route path="/logout/" component={Logout} />
+        <Route exact path={linkgen(Paths.home)} component={Home} />
+        <Route path={linkgen(Paths.login)} component={Login} />
+        <Route path={linkgen(Paths.signup)} component={Signup} />
+        <Route path={linkgen(Paths.logout)} component={Logout} />
       </Switch>
     </LayoutController>
   </BrowserRouter>
