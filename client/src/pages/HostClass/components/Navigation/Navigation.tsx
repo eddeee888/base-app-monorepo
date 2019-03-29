bf74083d1765d93d5fd7af4bb779a4411ef69ab2
Eddy Nguyen <eddeee.nguyen@gmail.com>
2019-03-30 10:50:32 +1100

    Add more contact details, add navigation

--- client/src/pages/HostClass/components/Navigation/Navigation.tsx
+++ client/src/pages/HostClass/components/Navigation/Navigation.tsx
@@ -1,13 +1,19 @@
 import Grid from '@material-ui/core/Grid';
+import { css } from 'emotion';
 import React from 'react';
 import Button from 'src/common/components/Button';
 import Link from 'src/common/components/Link';
+import { spacingRem } from 'src/common/helpers/spacing';
 import useHostClassNav from 'src/pages/HostClass/hooks/useHostClassNav';
+
+const containerClassName = css`
+  margin-top: ${spacingRem(2)}rem;
+`;
 
 const Navigation: React.FunctionComponent = () => {
   const { previous, next } = useHostClassNav();
   return (
-    <Grid container justify="space-between">
+    <Grid container justify="space-between" className={containerClassName}>
       <Grid item xs={4}>
         {previous && (
           <Link to={previous}>
