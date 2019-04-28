ad108756553dafff4bfbe46ab9c2cd99ec615eef
Eddy Nguyen <eddeee.nguyen@gmail.com>
2019-04-28 18:33:06 +1000

    Bring in hostAClass validation from learnd-utils 0.2.0

--- graphql/src/web/graphql/resolvers/mutations/classSave/classSave.ts
+++ graphql/src/web/graphql/resolvers/mutations/classSave/classSave.ts
@@ -1,12 +1,36 @@
+import { hostAClassValidation } from '@bit/eddeee888.learnd-utils.forms.validations';
+import { throwFormValidationError } from 'src/web/graphql/errors';
 import { MutationResolvers } from 'src/web/graphql/generated/graphqlgen';
 import { ClassSavePayload } from 'src/web/graphql/types';
 
 const classSave: MutationResolvers.ClassSaveResolver = async (
   parent,
-  args,
+  { input },
   ctx
 ) => {
-  throw new Error('implement me');
+  try {
+    await hostAClassValidation.details.validate({
+      name: input.name,
+      category: input.category,
+      description: input.description
+    });
+
+    await hostAClassValidation.contact.validate({
+      streetUnit: input.streetUnit,
+      streetAddress: input.streetAddress,
+      city: input.city,
+      postcode: input.postcode,
+      state: input.state,
+      country: input.country,
+      contactNumber: input.contactNumber
+    });
+
+    await hostAClassValidation.contact.validate({
+      sessions: [...input.sessions]
+    });
+  } catch (err) {
+    throwFormValidationError();
+  }
 
   const result: ClassSavePayload = {
     class: {
