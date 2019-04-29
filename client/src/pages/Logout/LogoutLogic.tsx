3574b5099c217122950d7bb5f9774c75acfbc392
Eddy Nguyen <eddeee.nguyen@gmail.com>
2019-04-29 18:31:50 +1000

    Update LogoutLogic to cover cases where accessToken might not be cleared properly

--- client/src/pages/Logout/LogoutLogic.tsx
+++ client/src/pages/Logout/LogoutLogic.tsx
@@ -1,20 +1,26 @@
 import { linkgen, Paths } from 'common/helpers/pathing';
 import React, { useEffect } from 'react';
+import { MutationFn } from 'react-apollo';
 import { Redirect } from 'react-router';
+import { Logout } from './__generated__/Logout';
 
 interface Props {
   clearViewer: () => void;
-  logout: () => void;
+  logout: MutationFn<Logout>;
 }
 
 const LogoutLogic: React.FunctionComponent<Props> = ({
   clearViewer,
   logout
 }) => {
-  useEffect(() => {
-    clearViewer();
-    logout();
-  }, []);
+  useEffect(
+    () => {
+      logout().then(() => {
+        clearViewer();
+      });
+    },
+    [logout, clearViewer]
+  );
 
   return <Redirect to={linkgen(Paths.home)} />;
 };
