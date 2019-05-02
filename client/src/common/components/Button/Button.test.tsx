1f9b0bfba4b3638b8a8c855e1d2fc4f3c8868732
Eddy Nguyen <eddeee.nguyen@gmail.com>
2019-05-02 19:38:28 +1000

    Update Button to handle loading state

--- client/src/common/components/Button/Button.test.tsx
+++ client/src/common/components/Button/Button.test.tsx
@@ -1,12 +1,22 @@
+import { CircularProgress } from '@material-ui/core';
 import { mount } from 'enzyme';
 import React from 'react';
 import Button from './Button';
 
 describe('<Button />', () => {
-  it('should match snapshot', () => {
+  it('should mount correctly without loading', () => {
     const wrapper = mount(<Button>Test button</Button>);
     expect(wrapper.html()).toMatch(/Test button/);
     expect(wrapper.find('button')).toHaveLength(1);
     expect(wrapper.find('button').props().className).toBeTruthy();
+    expect(wrapper.find(CircularProgress)).toHaveLength(0);
+  });
+
+  it('should mount correctly with loading', () => {
+    const wrapper = mount(<Button loading>Test button</Button>);
+    expect(wrapper.html()).toMatch(/Test button/);
+    expect(wrapper.find('button')).toHaveLength(1);
+    expect(wrapper.find('button').props().className).toBeTruthy();
+    expect(wrapper.find(CircularProgress)).toHaveLength(1);
   });
 });
