872af5625bacc92a89565c34099d0744043c720a
Eddy Nguyen <eddeee.nguyen@gmail.com>
2019-05-17 21:19:04 +1000

    Minor refinements (#3)
    Updates:
    
    Minor changes to docker-compose files
    Fix graphql setup to make sure it can generate ts out of the boxto make sure it can generate ts out of the box
    Update prisma model, client, server
    Update README

--- graphql/src/repositories/user/getUserByEmail.ts
+++ graphql/src/repositories/user/getUserByEmail.ts
@@ -1,6 +1,6 @@
 import { prisma, User } from 'src/web/graphql/generated/prisma-client';
 
-type GetByEmailFn = (email: string) => Promise<User>;
+type GetByEmailFn = (email: string) => Promise<User | null>;
 
 const getUserByEmail: GetByEmailFn = async email => {
   return await prisma.user({ email });
