dd8f69623ee90722019fba69581ea685e4ba3ad1
Eddy Nguyen <eddeee.nguyen@gmail.com>
2019-08-11 22:22:49 +1000

    Update reverse-proxy

--- reverse-proxy/entrypoint.sh
+++ reverse-proxy/entrypoint.sh
@@ -2,17 +2,15 @@
 
 # Replace 1 variable $ENV{<environment varname>}
 function replaceEnvVariable() {
-    grep -rl "\$ENV{$1}" $3|xargs -r \
-        sed -i "s|\\\$ENV{$1}|$2|g"
+    grep -rl "\$ENV{$1}" $3|xargs -r sed -i "s|\\\$ENV{$1}|$2|g"
 }
-
 # Replace ALL variables
 function replaceAllEnvVariables(){
     # Replace all environment variable in config file
     for _curVar in `env | awk -F = '{print $1}'`;do
         # awk has split them by the equals sign
         # Pass the name and value to our function
-        replaceEnvVariable ${_curVar} ${!_curVar} /etc/nginx/nginx.conf
+        replaceEnvVariable ${_curVar} ${!_curVar} /etc/nginx/conf.d/http.conf
     done
 }
 
