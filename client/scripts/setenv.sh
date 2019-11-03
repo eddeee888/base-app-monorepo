bb87da7ed22a00e71f043e32da09c6abcd900566
Eddy Nguyen <eddeee.nguyen@gmail.com>
2019-11-03 18:47:28 +1100

    Remove unrelated env variable wire up

--- client/scripts/setenv.sh
+++ client/scripts/setenv.sh
@@ -2,7 +2,6 @@ env_path=./.env
 
 function replaceEnvVariables(){
     sed -i -e "s~\\\$ENV{GRAPHQL_ENDPOINT}~${GRAPHQL_ENDPOINT}~g" ${env_path}
-    sed -i -e "s~\\\$ENV{GOOGLE_API_KEY}~${GOOGLE_API_KEY}~g" ${env_path}
 }
 
 function setupBit(){
