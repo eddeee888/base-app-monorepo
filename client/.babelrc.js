9cdac09108d7253e3227acc8e6bc03b0731c01e9
Eddy Nguyen <eddeee.nguyen@gmail.com>
2020-02-08 17:03:19 +0700

    Update non-src files for /client

---
+++ client/.babelrc.js
@@ -0,0 +1,34 @@
+const plugins = [
+  [
+    'babel-plugin-import',
+    {
+      libraryName: '@material-ui/core',
+      // Use "'libraryDirectory': ''," if your bundler does not support ES modules
+      libraryDirectory: 'esm',
+      camel2DashComponentName: false
+    },
+    'core'
+  ],
+  [
+    'babel-plugin-import',
+    {
+      libraryName: '@material-ui/icons',
+      // Use "'libraryDirectory': ''," if your bundler does not support ES modules
+      libraryDirectory: 'esm',
+      camel2DashComponentName: false
+    },
+    'icons'
+  ],
+  [
+    'babel-plugin-import',
+    {
+      libraryName: '@material-ui/lab',
+      // Use "'libraryDirectory': ''," if your bundler does not support ES modules
+      libraryDirectory: 'esm',
+      camel2DashComponentName: false
+    },
+    'lab'
+  ]
+];
+
+module.exports = { plugins };
--- client/scripts/install_gcg.sh
+++ client/scripts/install_gcg.sh
@@ -1,5 +1,4 @@
 function install_gcg() {
-  echo 
   yarn add -D \
     @graphql-codegen/typescript@$1 \
     @graphql-codegen/cli@$1 \
@@ -7,7 +6,8 @@ function install_gcg() {
     @graphql-codegen/near-operation-file-preset@$1 \
     @graphql-codegen/typescript-operations@$1 \
     @graphql-codegen/typescript-react-apollo@$1 \
-    @graphql-codegen/visitor-plugin-common@$1
+    @graphql-codegen/visitor-plugin-common@$1 \
+    @graphql-codegen/typescript-graphql-files-modules@$1
 }
 
 install_gcg $@
