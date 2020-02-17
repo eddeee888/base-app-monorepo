471b6a58c7e1b2de10d0fea8f5e90ca63b0c2af0
Eddy <eddy.nguyen@99designs.com>
2020-02-17 16:31:45 +1100

    Fix wrong fn creator used in routegen

--- client-seo/src/routes/RouteToMe.ts
+++ client-seo/src/routes/RouteToMe.ts
@@ -1,5 +1,5 @@
 /* This file was automatically generated and should not be edited. */
-import createRoute from 'src/common/pathing/createNextJsRoute';
+import createRoute from 'src/common/pathing/createExternalRoute';
 export interface RouteToMeParams {
 }
 const RouteToMe = createRoute<RouteToMeParams>('/app/me');
