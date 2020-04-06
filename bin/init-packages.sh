5d4bb3e30335633ddcc6d753c02903f298648ad3
Eddy Nguyen <eddeee.nguyen@gmail.com>
2020-04-06 22:46:53 +1000

    Install packages

--- bin/init-packages.sh
+++ bin/init-packages.sh
@@ -2,22 +2,8 @@
 
 function init_packages()
 {
-  echo "*** Installing packages in client..."
-  cd ./client && yarn
-  cd - 
-  echo ""
-  echo "---"
-
-  echo "*** Installing packages in client-seo..."
-  cd ./client-seo && yarn
-  cd - 
-  echo ""
-  echo "---"
-
-  echo "*** Installing packages and build in graphql..."
-  cd ./server && yarn && yarn gg && yarn prisma:generate && yarn build
-  cd -
-  echo ""
+  echo "*** Installing packages..."
+  yarn install
   echo "---"
 }
 
