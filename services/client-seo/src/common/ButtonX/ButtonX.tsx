e4afdb747fa71b5b42b74c77c73f56621b79cad5
Eddy Nguyen <eddeee.nguyen@gmail.com>
2021-05-03 21:17:52 +1000

    Update client-seo's Link, ButtonX wireup, ViewerMenu to use ButtonX and routeConfig

---
+++ services/client-seo/src/common/ButtonX/ButtonX.tsx
@@ -0,0 +1,23 @@
+import { FunctionComponent } from "react";
+import { generateUrl } from "@route-codegen/utils";
+import Button, { ButtonProps } from "~/common/shared-ui/Button";
+import { routeConfig, RouteConfigProps } from "~/routes/routeConfig";
+
+export type ButtonXProps = Omit<ButtonProps, "href"> & RouteConfigProps;
+
+const ButtonX: FunctionComponent<ButtonXProps> = ({ to, urlParams, ...props }) => {
+  const { pathPattern, Component } = routeConfig[to];
+  const href = generateUrl(pathPattern, {
+    path: urlParams && "path" in urlParams ? urlParams.path : {},
+    query: urlParams?.query,
+    origin: urlParams?.origin,
+  });
+
+  return (
+    <Component href={href}>
+      <Button {...props} component="span" />
+    </Component>
+  );
+};
+
+export default ButtonX;
--- services/client-seo/src/common/components/Link/Link.tsx
+++ services/client-seo/src/common/components/Link/Link.tsx
@@ -2,27 +2,26 @@ import { FunctionComponent, forwardRef, Ref, ReactNode } from "react";
 import { Link as LinkMui, LinkProps as LinkMuiProps } from "@material-ui/core";
 import LinkNext, { LinkProps as LinkNextProps } from "next/link";
 
-export interface LinkProps extends Omit<LinkNextProps, "href"> {
-  innerRef?: Ref<any>;
+export interface LinkProps extends LinkNextProps {
+  innerRef?: Ref<HTMLAnchorElement>;
   children: ReactNode;
   color?: LinkMuiProps["color"];
-  nextHref: LinkNextProps["href"];
 }
 
-const NextComposed = forwardRef<any, Omit<LinkProps, "innerRef">>(function NextComposed(props, ref) {
-  const { as, nextHref, prefetch, ...other } = props;
+const NextComposed = forwardRef<HTMLAnchorElement, Omit<LinkProps, "innerRef">>(function NextComposed(props, ref) {
+  const { as, href, prefetch, ...other } = props;
 
   return (
-    <LinkNext href={nextHref} prefetch={prefetch} as={as}>
+    <LinkNext href={href} prefetch={prefetch} as={as}>
       <a ref={ref} {...other} />
     </LinkNext>
   );
 });
 
-const Link: FunctionComponent<LinkProps> = ({ innerRef, ...props }) => {
-  return <LinkMui underline="none" color="primary" component={NextComposed} {...props} ref={innerRef} />;
+const Link: FunctionComponent<LinkProps> = ({ href, innerRef, ...props }) => {
+  return <LinkMui underline="none" color="primary" href={href as string} component={NextComposed} {...props} ref={innerRef} />;
 };
 
-export default forwardRef<any, LinkProps>(function LinkWithref(props, ref) {
+export default forwardRef<HTMLAnchorElement, LinkProps>(function LinkWithref(props, ref) {
   return <Link {...props} innerRef={ref} />;
 });
