dfdee4069eea1550461a3c593e2dd77c09be3f94
Eddy Nguyen <eddeee.nguyen@gmail.com>
2021-05-03 18:05:43 +1000

    Add ButtonX implementation

--- services/client/src/common/components/ButtonX/ButtonX.tsx
+++ services/client/src/common/components/ButtonX/ButtonX.tsx
@@ -1,11 +1,11 @@
+import { FunctionComponent } from "react";
 import { generateUrl } from "@route-codegen/utils";
-import React from "react";
 import Button, { ButtonProps } from "~/common/shared-ui/Button";
 import { routeConfig, RouteConfigProps } from "~/routes/routeConfig";
 
 export type ButtonXProps = Omit<ButtonProps, "href"> & RouteConfigProps;
 
-const ButtonX: React.FunctionComponent<ButtonXProps> = ({ to, urlParams = {}, ...props }) => {
+const ButtonX: FunctionComponent<ButtonXProps> = ({ to, urlParams = {}, ...props }) => {
   const { pattern, component: Link } = routeConfig[to];
   const href = generateUrl(pattern, urlParams as any);
 
