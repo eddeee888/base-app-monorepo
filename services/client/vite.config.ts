5832031269401800c36c6a75ea2051eb0f66f05f
Eddy Nguyen <eddeee.nguyen@gmail.com>
2021-07-13 00:24:40 +1000

    Fix tsconfig and vite build, analysing options

--- services/client/vite.config.ts
+++ services/client/vite.config.ts
@@ -6,9 +6,12 @@ import reactJsx from "vite-react-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  clearScreen: false,
-  plugins: [reactRefresh(), tsconfigPaths(), graphql(), reactJsx()],
+  build: {
+    sourcemap: true,
+  },
   server: {
     port: parseInt(process.env.DEV_SERVER_PORT) || 3000,
   },
+  plugins: [reactRefresh(), tsconfigPaths(), graphql(), reactJsx()],
+  clearScreen: false,
 });
