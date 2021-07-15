38173f9e71af47992ef47d2f87c84fffccf6bcd9
Eddy Nguyen <eddeee.nguyen@gmail.com>
2021-07-15 22:52:12 +1000

    Fix import path Link component in client

--- services/client/src/routes/routeConfig.ts
+++ services/client/src/routes/routeConfig.ts
@@ -1,5 +1,5 @@
 /* This file was automatically generated with route-codegen and should not be edited. */
-import { Link } from "@/common";
+import { Link } from "@/common/Link";
 import Anchor from "@/shared/ui/Anchor";
 import { UrlParamsMe, patternMe } from "./me/patternMe";
 import { UrlParamsHome, patternHome } from "./home/patternHome";
