b49c294702ed01548f957f9d7e0625496971d951
Eddy Nguyen <eddeee.nguyen@gmail.com>
2021-07-15 23:33:15 +1000

    Fix source location of error-exit.sh

--- bin/utils/error-exit.sh
+++ bin/utils/error-exit.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-source $CURRENT_DIR/constants.sh
-
 function error_exit {
-    echo -e "=> ${CORE_CMD_NAME}.error: $1" >&2   ## Send message to stderr. Exclude >&2 if you don't want it that way.
-    exit "${2:-1}"  ## Return a code specified by $2 or 1 by default.
+  local CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+  source $CURRENT_DIR/constants.sh
+
+  echo -e "=> ${CORE_CMD_NAME}.error: $1" >&2   ## Send message to stderr. Exclude >&2 if you don't want it that way.
+  exit "${2:-1}"  ## Return a code specified by $2 or 1 by default.
 }
