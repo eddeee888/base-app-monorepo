a54c4760c99910155ee5d05ed2c25b9715c35121
Eddy Nguyen <eddeee.nguyen@gmail.com>
2021-07-16 20:21:29 +1000

    Update code.sh to open root directory

--- bin/code.sh
+++ bin/code.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-source $CURRENT_DIR/utils/constants.sh
-
 function main(){
-  local cmd="${IDE_CMD} ./services/$1"
+  CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+  source $CURRENT_DIR/utils/constants.sh
+
+  case $1 in
+    "")
+      local cmd="${IDE_CMD} ."
+    ;;
+    *)
+      local cmd="${IDE_CMD} ./services/$1"
+    ;;
+  esac
+
   echo "=> ${CORE_CMD_NAME}.code: "$cmd
   eval $cmd
 }
