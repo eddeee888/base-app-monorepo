d998d5793e019ce3bb05aa7f51a9b403773384fd
Eddy Nguyen <ch@eddeee888.me>
2022-09-11 22:26:27 +1000

    Bump cypress to 10.7.0

---
+++ apps/main-e2e/cypress.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'cypress';
+import { nxE2EPreset } from '@nrwl/cypress/plugins/cypress-preset';
+
+const cypressJsonConfig = {
+  baseUrl: 'http://localhost:3000',
+  fileServerFolder: '.',
+  fixturesFolder: './src/fixtures',
+  video: true,
+  videosFolder: '../../dist/cypress/apps/main-e2e/videos',
+  screenshotsFolder: '../../dist/cypress/apps/main-e2e/screenshots',
+  chromeWebSecurity: false,
+  specPattern: 'src/e2e/**/*.cy.{js,jsx,ts,tsx}',
+  supportFile: 'src/support/e2e.ts',
+};
+export default defineConfig({
+  e2e: {
+    ...nxE2EPreset(__dirname),
+    ...cypressJsonConfig,
+  },
+});
