a1c94d66bf9e9e4bee0a61e364fefa8ff212e6ee
Eddy Nguyen <ch@eddeee888.me>
2022-09-23 16:42:43 +1000

    Revert "TEST ci.yml correctly saves failed cypress artifacts"
    This reverts commit 79fbd8fd66a168563e6881cebc1dd5c51a5543bb.

--- apps/main-e2e/src/e2e/app.cy.ts
+++ apps/main-e2e/src/e2e/app.cy.ts
@@ -2,7 +2,7 @@ describe('main', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
-    cy.get('h1').contains('Welcomessssss!');
+    cy.get('h1').contains('Welcome!');
     cy.contains('User list:');
   });
 });
