79fbd8fd66a168563e6881cebc1dd5c51a5543bb
Eddy Nguyen <ch@eddeee888.me>
2022-09-23 16:34:40 +1000

    TEST ci.yml correctly saves failed cypress artifacts

--- apps/main-e2e/src/e2e/app.cy.ts
+++ apps/main-e2e/src/e2e/app.cy.ts
@@ -2,7 +2,7 @@ describe('main', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
-    cy.get('h1').contains('Welcome!');
+    cy.get('h1').contains('Welcomessssss!');
     cy.contains('User list:');
   });
 });
