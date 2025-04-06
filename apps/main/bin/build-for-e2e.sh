2de793145ef2f40e548e32627e2e7c5b6b236396
Eddy Nguyen <ch@eddeee888.me>
2025-04-06 19:20:52 +1000

    Update 20250406 (#137)
    * Fix build steps
    
    * Fix command

--- apps/main/bin/build-for-e2e.sh
+++ apps/main/bin/build-for-e2e.sh
@@ -3,18 +3,19 @@
 set -e
 
 function main() {
-  nx codegen main-prisma
-  nx graphql-codegen graph-backend
+  # Prepare Prisma package and files
+  yarn nx codegen main-prisma
+  yarn nx build main-prisma
 
-  nx build main
+  yarn nx build main
 
   cp apps/main/.env.e2e dist/apps/main/.env
   cp -R node_modules/ dist/apps/main/node_modules
 
   # Prepare Prisma package and files
-  mkdir -p dist/apps/main/node_modules/@bam
-  cp -R dist/libs/main-prisma/ dist/apps/main/node_modules/@bam/main-prisma
-  cp -R libs/main-prisma/src/prisma/generated/ dist/apps/main/node_modules/@bam/main-prisma/src/prisma/generated/
+  mkdir -p dist/apps/main/node_modules/@airfive
+  cp -R dist/libs/main-prisma/ dist/apps/main/node_modules/@airfive/main-prisma
+  cp -R libs/main-prisma/src/prisma/generated/ dist/apps/main/node_modules/@airfive/main-prisma/src/prisma/generated/
 }
 
 main
